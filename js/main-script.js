f475aecb5d617f8611f5ac50c4791c9e7c7984a1
Thato Mokhemisa <thato@tshimologong.joburg>
2020-06-22 17:36:41 +0200

    Initial Commit

---
+++ js/main-script.js
@@ -0,0 +1,18 @@
+// 'use strict';
+
+// (function($, sr) {
+//     jQuery.fn[sr] = function(fn) {
+//         return fn ? this.bind('resize', debounce(fn)) : this.trigger(sr);
+//     };
+// })
+// (jQuery, 'smartresize');
+
+// $(function() {
+//     //Fix main section height
+//     let setMainBanner = function() {
+//         let mainHeight = $(window).height();
+
+//         $('#overlay').height(mainHeight);
+//     }
+//     setMainBanner();
+// });
